a297a78e0940944849d78de178b0c9f9fe2fc00c
zhishaofei <zhishaofei@jd.com>
2024-08-26 15:56:43 +0800

    feat: custom event

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  window.dataLayer = window.dataLayer || []
+
+  const handleClick = () => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'reactCustomEvent', // 这个名字要和你在 GTM 中定义的事件名称一致
+      category: 'Button',        // 自定义参数，选择性添加
+      action: 'Click',
+      label: 'Submit Button'
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleClick}>Submit</button>
       </header>
     </div>
   );
